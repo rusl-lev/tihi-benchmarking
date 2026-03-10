69bf44a445fd551b925136187c7f020fd7e2cb1b
Ruslan Levochkin <ruslan.t.levochkin@gmail.com>
2026-03-10 18:26:51 +0400

    Update distributions.py

--- src/tihi/tihi_utils/distributions.py
+++ src/tihi/tihi_utils/distributions.py
@@ -122,8 +122,8 @@ class LorentzianFitter():
         self.centers = peaks[:,0]
         self.amplitudes = peaks[:,1]
         self.gammas = [1]*len(self.centers)
-        self.params = np.array([self.centers, self.amplitudes, self.gammas]).T
-        self.start_params = self.params.flatten().tolist()
+        self.params = np.array([self.centers, self.amplitudes, self.gammas])
+        self.start_params = self.params.tolist()
         self.decompositions = []
         self.output_params = np.zeros(shape=(self.centers.shape[0], 3))
 
@@ -132,7 +132,7 @@ class LorentzianFitter():
             x_lb = self.bounds[i]
             allowed_dev = (x_ub - x_lb) * self.peak_position_tolerance
             peak = self.centers[i]
-            peak_deviation_bound = ([peak - allowed_dev, -np.inf, -np.inf], [peak + allowed_dev, np.inf, np.inf])
+            peak_deviation_bound = ([(peak - allowed_dev), -np.inf, -np.inf], [(peak + allowed_dev), np.inf, np.inf])
             mask = (self.x_vals >= x_lb) & (self.x_vals <= x_ub)
             x_masked = self.x_vals[mask]
             y_masked = self.y_vals[mask]
