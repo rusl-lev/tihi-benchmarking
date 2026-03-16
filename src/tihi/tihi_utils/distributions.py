Fixed Lorentz function calculation

src/tihi/tihi_utils/distributions.py
@@ -186,7 +186,8 @@ class LorentzianFitter():
         :param gamma (float) : Full width at half maximum (FWHM) of the Lorentzian function.
         :return (ndarray) : Calculated Lorentzian function values.
         """
-        amplitude = amplitude * (-1.0)
+        # amplitude = amplitude * (-1.0)
+        gamma /= 2
         return amplitude * (gamma / np.pi) / ((x - center) ** 2 + gamma ** 2)
 
     def lorentzian_sum(self, x, params):